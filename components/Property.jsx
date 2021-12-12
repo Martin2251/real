86f28db2561707ea61d8c33f4d76f0acc243b293
martin lowe <martinlowe673@gmail.com>
2021-12-12 11:54:35 +0000

    rent frequency

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,10 +6,24 @@ import {BsGridFill} from "react-icons/bs"
 import {GoVerified } from "react-icons/go"
 import millify from "millify"
 
+import DefaultImage from "../assets/images/road.jpg"
+
 const Property = ({property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId}}) => (
     <Link href={"/property/${externalId}"} passHref>
-        <Flex flexWrap="wrap">
+        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+    <Box>
+        <Image src={coverPhoto ? coverPhoto.url :DefaultImage} width={400} height={260} alt="house"/>
+    </Box>
+  <Box w="full">
+    <Flex paddingTop="2" alignItems="centrer" justifyContent="space-between">
+    <Flex alignItems="center">
+    <Box paddingRight="3" color="green.400">{isVerified && <GoVerified />}
 
+    </Box>
+    <Text fontWeight="bold" fontSize="lg">AED {price}{rentFrequency && `/${rentFrequency}`}</Text>
+    </Flex>
+    </Flex>
+  </Box>
         </Flex>
     </Link>
 )
